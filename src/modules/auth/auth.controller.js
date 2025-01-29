fffaef69514289bb9db2147cac97d5f056f0ab81
eidabdallah <eidabdallah971@gmail.com>
2025-01-29 06:50:33 +0200

    update login function

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -5,20 +5,20 @@ import { sendEmail } from "../../utils/sendEmail.js";
 import { customAlphabet } from "nanoid";
 
 export const register = async (req, res, next) => {
-    const { userName, email, password, phoneNumber, address } = req.body;
-    const user = await userModel.findOne({ email });
-    if (user)
-        return res.status(409).json({ message: 'User already exists' });
-    const checkPhone = await userModel.findOne({ phoneNumber });
-    if (checkPhone)
-        return res.status(409).json({ message: 'Phone number already exists' });
+  const { userName, email, password, phoneNumber, address } = req.body;
+  const user = await userModel.findOne({ email });
+  if (user)
+    return res.status(409).json({ message: 'User already exists' });
+  const checkPhone = await userModel.findOne({ phoneNumber });
+  if (checkPhone)
+    return res.status(409).json({ message: 'Phone number already exists' });
 
-    const hashPassword = bcrypt.hashSync(password, parseInt(process.env.SALTROUND));
-    const createUser = await userModel.create({ userName, email, password: hashPassword, phoneNumber, address });
+  const hashPassword = bcrypt.hashSync(password, parseInt(process.env.SALTROUND));
+  const createUser = await userModel.create({ userName, email, password: hashPassword, phoneNumber, address });
 
-    const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: 60 * 5 });
-    const refreshToken = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    const html = `
+  const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: 60 * 5 });
+  const refreshToken = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  const html = `
    <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px; background-color: #f9f9f9;">
   <div style="max-width: 600px; margin: auto; background: linear-gradient(135deg, #007bff, #6c757d); padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
     <div style="border-bottom: 1px solid #eee; padding-bottom: 15px; margin-bottom: 20px;">
@@ -53,40 +53,43 @@ export const register = async (req, res, next) => {
   </div>
 </div>
 `;
-    await sendEmail(email, 'Confirm Email', html);
-    res.status(201).json({ message: 'User registered successfully', user: createUser });
+  await sendEmail(email, 'Confirm Email', html);
+  res.status(201).json({ message: 'User registered successfully', user: createUser });
 }
 export const login = async (req, res, next) => {
-    const { email, password } = req.body;
-    const user = await userModel.findOne({ email });
-    if (!user)
-        return res.status(400).json({ message: 'Invalid credentials' });
-    if (!user.confirmEmail)
-        return res.status(403).json({ message: 'Please confirm your email' });
-    const isMatch = bcrypt.compareSync(password, user.password);
-    if (!isMatch)
-        return res.status(400).json({ message: 'Invalid credentials' });
-    const token = jwt.sign({ id: user._id, role: user.role, status: user.status }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    return res.status(200).json({ message: 'Logged in successfully', token });
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user)
+    return res.status(400).json({ message: 'Invalid credentials' });
+  if (!user.confirmEmail)
+    return res.status(403).json({ message: 'Please confirm your email' });
+
+  const isMatch = bcrypt.compareSync(password, user.password);
+  if (!isMatch)
+    return res.status(400).json({ message: 'Invalid credentials' });
+  if (user.status == 'NotActive')
+    return res.status(400).json({ message: 'Your Account is Blocked' });
+  const token = jwt.sign({ id: user._id, role: user.role, status: user.status }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return res.status(200).json({ message: 'Logged in successfully', token });
 }
-export const confirmEmail = async (req, res , next) => {
-    const { token } = req.params;
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await userModel.updateOne({ email: decodedToken.email }, { confirmEmail: true });
-    if (user.modifiedCount > 0) {
-        return res.redirect("http://localhost:5173/auth");
-    }
+export const confirmEmail = async (req, res, next) => {
+  const { token } = req.params;
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await userModel.updateOne({ email: decodedToken.email }, { confirmEmail: true });
+  if (user.modifiedCount > 0) {
+    return res.redirect("http://localhost:5173/auth");
+  }
 }
 
-export const sendCode = async (req , res , next) => {
-    const { email } = req.body;
-    const user = await userModel.findOne({ email });
-    if (!user)
-        return res.status(404).json({ message: 'Email not found' });
-    const code = customAlphabet('123456789abcdefghijklmnopqrstuvwxyz' , 6)();
-    user.sendCode = code;
-    await user.save();
-    const html = `<div style="font-family: Arial, sans-serif; text-align: center; padding: 20px; background-color: #f9f9f9;">
+export const sendCode = async (req, res, next) => {
+  const { email } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user)
+    return res.status(404).json({ message: 'Email not found' });
+  const code = customAlphabet('123456789abcdefghijklmnopqrstuvwxyz', 6)();
+  user.sendCode = code;
+  await user.save();
+  const html = `<div style="font-family: Arial, sans-serif; text-align: center; padding: 20px; background-color: #f9f9f9;">
     <div style="max-width: 600px; margin: auto; background: linear-gradient(135deg, #007bff, #6c757d); padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
       <div style="border-bottom: 1px solid #eee; padding-bottom: 15px; margin-bottom: 20px;">
         <div style="display: inline-block; border-radius: 50%; padding: 20px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
@@ -111,35 +114,35 @@ export const sendCode = async (req , res , next) => {
       <hr style="border: none; border-top: 1px solid #6c757d; margin: 20px 0;" />
     </div>
   </div>`;
-    await sendEmail(email, 'Reset Password', html);
-    res.status(200).json({ message: 'Code sent successfully' });
+  await sendEmail(email, 'Reset Password', html);
+  res.status(200).json({ message: 'Code sent successfully' });
 }
 
-export const forgetPassword = async(req, res , next) => {
-    const { email , password , code } = req.body;
-    const user = await userModel.findOne({ email });
-    if (!user)
-        return res.status(404).json({ message: 'Email not found' });
-    if(user.sendCode != code){
-        return res.status(403).json({ message: 'Invalid code' });
-    }
-    user.password = bcrypt.hashSync(password, parseInt(process.env.SALTROUND));
-    user.sendCode = null;
-    await user.save();
-    res.status(200).json({ message: 'Password reset successfully' });
+export const forgetPassword = async (req, res, next) => {
+  const { email, password, code } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user)
+    return res.status(404).json({ message: 'Email not found' });
+  if (user.sendCode != code) {
+    return res.status(403).json({ message: 'Invalid code' });
+  }
+  user.password = bcrypt.hashSync(password, parseInt(process.env.SALTROUND));
+  user.sendCode = null;
+  await user.save();
+  res.status(200).json({ message: 'Password reset successfully' });
 }
 
-export const changePassword = async (req , res , next) => {
-    const { email , oldPassword , newPassword } = req.body;
-    if(req.user.email != email)
-        return res.status(403).json({ message: 'The email address provided does not match your account.' });
-    const user = await userModel.findOne({ email });
-    if (!user)
-        return res.status(404).json({ message: 'Email not found' });
-    const isMatch = bcrypt.compareSync(oldPassword, user.password);
-    if (!isMatch)
-        return res.status(403).json({ message: 'Invalid old password' });
-    user.password = bcrypt.hashSync(newPassword, parseInt(process.env.SALTROUND));
-    await user.save();
-    res.status(200).json({ message: 'Password changed successfully' });
+export const changePassword = async (req, res, next) => {
+  const { email, oldPassword, newPassword } = req.body;
+  if (req.user.email != email)
+    return res.status(403).json({ message: 'The email address provided does not match your account.' });
+  const user = await userModel.findOne({ email });
+  if (!user)
+    return res.status(404).json({ message: 'Email not found' });
+  const isMatch = bcrypt.compareSync(oldPassword, user.password);
+  if (!isMatch)
+    return res.status(403).json({ message: 'Invalid old password' });
+  user.password = bcrypt.hashSync(newPassword, parseInt(process.env.SALTROUND));
+  await user.save();
+  res.status(200).json({ message: 'Password changed successfully' });
 }
