1853862ef674e86e02a3538c2534117c5384a2a5
eidabdallah <eidabdallah971@gmail.com>
2025-02-17 19:52:32 +0200

    update auth middleware error message to appError

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import userModel from './../../DB/model/user.model.js';
+import { AppError } from './../utils/AppError.js';
 
 export const roles = {
     ADMIN: 'Admin',
@@ -9,19 +10,19 @@ export const auth = (accessRoles = []) => {
     return async (req, res, next) => {
         const { authorization } = req.headers;
         if (!authorization?.startsWith(process.env.BEARERKEY)) {
-            return res.status(401).json({ message: "Invalid token" });
+            return next(new AppError("Invalid token", 401));
         }
         const token = authorization.split(process.env.BEARERKEY)[1];
         const decode = jwt.verify(token, process.env.JWT_SECRET);
         if (!decode) {
-            return res.status(401).json({ message: "Invalid token" });
+            return next(new AppError("Invalid token", 401));
         }
         const user = await userModel.findById(decode.id).select('userName role email');
         if (!user) {
-            return res.status(404).json({ message: "User not found" });
+            return next(new AppError("User not found", 404));
         }
         if (!accessRoles.includes(user.role)) {
-            return res.status(403).json({ message: "Unauthorized access" });
+            return next(new AppError("Unauthorized access", 403));
         }
 
         req.user = user;
