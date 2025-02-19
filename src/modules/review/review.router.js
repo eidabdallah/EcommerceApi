87969562a8820769c7c35a3d13ffede51f65f682
eidabdallah <eidabdallah971@gmail.com>
2025-02-19 10:24:50 +0200

    create review validation

--- src/modules/review/review.router.js
+++ src/modules/review/review.router.js
@@ -4,8 +4,11 @@ import { auth } from "../../middleware/auth.middleware.js";
 import { endPoints } from "./review.role.js";
 import { fileMimeTypes, fileUpload } from './../../utils/multer.js';
 import { asyncHandler } from "../../utils/asyncHandler.js";
+import * as schema from "./review.validation.js";
+import { validation } from "../../middleware/validation.middleware.js";
+
 const router = Router({ caseSensitive: true , mergeParams: true});
 
-router.post('/' , asyncHandler(auth(endPoints.create)) ,fileUpload(fileMimeTypes.image).single('image'), asyncHandler(reviewController.createReview));
+router.post('/' , fileUpload(fileMimeTypes.image).single('image'), validation(schema.createReviewSchema) , asyncHandler(auth(endPoints.create)) , asyncHandler(reviewController.createReview));
 
 export default router;
