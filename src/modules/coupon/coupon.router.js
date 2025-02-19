6817db2fd38a9bf8ddc505e5e71f1fac75cb814a
eidabdallah <eidabdallah971@gmail.com>
2025-02-19 09:52:30 +0200

    create coupon validation

--- src/modules/coupon/coupon.router.js
+++ src/modules/coupon/coupon.router.js
@@ -3,13 +3,16 @@ import * as couponController from "./coupon.controller.js";
 import { auth } from "../../middleware/auth.middleware.js";
 import { endPoints } from "./coupon.role.js";
 import { asyncHandler } from './../../utils/asyncHandler.js';
-const router = Router({ caseSensitive: true });
+import * as schema from "./coupon.validation.js";
+import { validation } from "../../middleware/validation.middleware.js";
 
-router.post('/', asyncHandler(auth(endPoints.create)), asyncHandler(couponController.createCoupon));
+const router = Router({ caseSensitive: true });
+ 
+router.post('/', validation(schema.createCouponSchema) , asyncHandler(auth(endPoints.create)), asyncHandler(couponController.createCoupon));
 router.get('/', asyncHandler(auth(endPoints.getAll)), asyncHandler(couponController.getAllCoupons));
-router.patch('/:id' , asyncHandler(auth(endPoints.update)), asyncHandler(couponController.updateCoupon));
-router.get('/:id' , asyncHandler(auth(endPoints.getById)) , asyncHandler(couponController.getCouponById));
-router.delete('/:id' , asyncHandler(auth(endPoints.delete)) , asyncHandler(couponController.deleteCoupon));
+router.patch('/:id' ,validation(schema.updateCouponSchema), asyncHandler(auth(endPoints.update)), asyncHandler(couponController.updateCoupon));
+router.get('/:id' , validation(schema.getCouponsByIdSchema) , asyncHandler(auth(endPoints.getById)) , asyncHandler(couponController.getCouponById));
+router.delete('/:id' , validation(schema.deleteCouponSchema), asyncHandler(auth(endPoints.delete)) , asyncHandler(couponController.deleteCoupon));
 
 
 export default router;
